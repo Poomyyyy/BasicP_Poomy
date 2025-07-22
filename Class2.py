307c34664dc75705e8fee2e2ab4a794210a0bbe2
Poomyyyy <8pitipoomsaripan@gmail.com>
2025-07-22 22:13:11 +0700

    Update New Class 2 in Same Workshop

--- Class2.py
+++ Class2.py
@@ -1,1 +1,16 @@
 # loading....
+distance = int(input("Input the distance : "))
+sum = 0
+if distance < 5 :
+    print("No Servrice")
+elif distance < 50 :
+    sum += 10
+elif distance < 100 :
+    sum += 15
+elif distance < 300 :
+    sum += 25
+elif distance < 500 :
+    sum += 35
+else:
+    sum == 45
+print(sum)

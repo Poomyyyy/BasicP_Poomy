6bdc60f8a9bad2f66ae25dae09eed974cd0b611d
poomyy <8pitipoomsaripan@gmail.com>
2025-08-03 17:31:53 +0700

    mini test

--- Class4_Workshop.py
+++ Class4_Workshop.py
@@ -1,13 +1,10 @@
 movie = [
         {'movie_name': 'F1', 'ticket_price': 200, 'genre': 'action', 'age_restriction': '13'},
         {'movie_name': 'Yourname', 'ticket_price': 300, 'genre': 'romatic', 'age_restriction': '5'},
-        {'movie_name': 'top gun', 'ticket_price': 500, 'genre': 'action', 'age_restriction': '15'},
+        {'movi7777e_name': 'top gun', 'ticket_price': 500, 'genre': 'action', 'age_restriction': '15'},
         {'movie_name': 'Citizen Dog', 'ticket_price': 150, 'genre': 'romatic', 'age_restriction': '13'},
         {'movie_name': 'sonic the hedgehog 1', 'ticket_price': 450, 'genre': 'comedy', 'age_restriction': '5'},
     ]
-
-#def checkage():
-
 #def calculateprice():
 
 #def buy_tick():
@@ -18,18 +15,20 @@ def main():
     if menue == '1':
         show_mov()
     elif menue == '2':
-        print("test")
+        buy_tick()
     else:
         print("เมนูไม่ถูกต้อง")
-
-def show_mov():
+        break
+def show_mov(movie_list):
     for name_mov in movie :
         print(name_mov["movie_name"])
 def buy_tick():
-    show_mov()
+    show_mov(movie_list)
     userinput = input("เลือกลำดับหนัง : ")
     userage = int(input("อายุของคุณ : "))
-#def checkage(userage, age_restriction):
-    #if userage
-    #if test
+def checkage(userage, age_restriction):
+    if userage > 10:
+        print("Pass")
+        return
+    elif userage < 10:
 main()

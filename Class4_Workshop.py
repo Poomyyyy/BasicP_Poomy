0a01251e96147160fafc7f33dc74c7da85749dea
Poomyyyy <8pitipoomsaripan@gmail.com>
2025-08-04 11:39:48 +0700

    60% Completed

--- Class4_Workshop.py
+++ Class4_Workshop.py
@@ -1,34 +1,35 @@
 movie = [
-        {'movie_name': 'F1', 'ticket_price': 200, 'genre': 'action', 'age_restriction': '13'},
-        {'movie_name': 'Yourname', 'ticket_price': 300, 'genre': 'romatic', 'age_restriction': '5'},
-        {'movi7777e_name': 'top gun', 'ticket_price': 500, 'genre': 'action', 'age_restriction': '15'},
-        {'movie_name': 'Citizen Dog', 'ticket_price': 150, 'genre': 'romatic', 'age_restriction': '13'},
-        {'movie_name': 'sonic the hedgehog 1', 'ticket_price': 450, 'genre': 'comedy', 'age_restriction': '5'},
-    ]
+    {'movie_name': 'F1', 'ticket_price': 200, 'genre': 'action', 'age_restriction': 13},
+    {'movie_name': 'Yourname', 'ticket_price': 300, 'genre': 'romatic', 'age_restriction': 5},
+    {'movie_name': 'top gun', 'ticket_price': 500, 'genre': 'action', 'age_restriction': 15},
+    {'movie_name': 'Citizen Dog', 'ticket_price': 150, 'genre': 'romatic', 'age_restriction': 13},
+    {'movie_name': 'sonic the hedgehog 1', 'ticket_price': 450, 'genre': 'comedy', 'age_restriction': 'G'},
+]
 #def calculateprice():
 
-#def buy_tick():
+def buy_tick():
+    show_mov()
+    userinput = int(input("เลือกลำดับหนัง"))
+    userage = input("อายุของคุณ : ")
+    checkage(userinput, userage)
 
-def main():
-    print("กด 1 เพื่อ แสดงหนังทั้งหมด กด 2 เพื่อซื้อตั๋วหนัง")
-    menue = input("เลือกเมนู : ")
-    if menue == '1':
-        show_mov()
-    elif menue == '2':
-        buy_tick()
+def checkage(userinput, userage):
+    if movie[userinput - 1]['age_restriction'] == 'G':
+        print("OKKKKK")
+        return
     else:
-        print("เมนูไม่ถูกต้อง")
-        break
-def show_mov(movie_list):
+        print("KAK")
+def show_mov():
     for name_mov in movie :
         print(name_mov["movie_name"])
-def buy_tick():
-    show_mov(movie_list)
-    userinput = input("เลือกลำดับหนัง : ")
-    userage = int(input("อายุของคุณ : "))
-def checkage(userage, age_restriction):
-    if userage > 10:
-        print("Pass")
-        return
-    elif userage < 10:
+def main():
+    while True:
+        print("กด 1 เพื่อ แสดงหนังทั้งหมด กด 2 เพื่อซื้อตั๋วหนัง")
+        menue = input("เลือกเมนู : ")
+        if menue == '1':
+            show_mov()
+        elif menue == '2':
+            buy_tick()
+        else:
+            print("เมนูไม่ถูกต้อง")
 main()
